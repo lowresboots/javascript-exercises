01a2a2e025b2321748e4a597929beb8136b485e4
lowresboots <lowresboots@gmail.com>
2024-10-04 03:27:15 -0400

    10_fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
 
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    if (n === 0) {
+        return 0;
+    }
+
+    let a = 1;
+    let b = 1;
+
+    if (n ===1 || n === 2) {
+        return 1;
+    }
+
+    for (let i = 3; i<= n; i++) {
+        const temp = a + b;
+        a = b;
+        b = temp;
+    }
+    
+    return b;
 };
 
 // Do not edit below this line
