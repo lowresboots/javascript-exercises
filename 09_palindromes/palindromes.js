8d2b926f4ad78c30983d808a93b31609731bddc9
lowresboots <lowresboots@gmail.com>
2024-10-04 02:51:00 -0400

    09_palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reversedStr = cleanStr.split('').reverse().join('');
 
+    return cleanStr === reversedStr;
 };
 
 // Do not edit below this line
